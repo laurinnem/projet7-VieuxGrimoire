3561e9162e060879cdf0e1a498dffcd790c9bcd0
laurinnem <122321024+laurinnem@users.noreply.github.com>
2023-09-21 18:05:44 +0200

    add models & routes for book

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,5 +1,67 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const Book = require("./models/Book");
+const User = require("./models/User");
+
+mongoose
+  .connect("mongodb+srv://laurinnem:bcoymMC7@cluster0.i4sqyt7.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/Book", (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body,
+  });
+  book
+    .save()
+    .then(() => res.status(201).json({ message: "livre enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/Book/:id", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "livre modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/Book/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "livre supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/Book/:id", (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.get("/api/Book", (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+});
 
 module.exports = app;
