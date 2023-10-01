a20c1bb8ecce4bf49b4851208feeb9b8e37a23eb
laurinnem <122321024+laurinnem@users.noreply.github.com>
2023-10-01 12:20:28 +0200

    add multer and file management

--- backEnd/routes/book.js
+++ backEnd/routes/book.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const auth = require("../middleware/auth");
 const router = express.Router();
+
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 const bookController = require("../controllers/book");
 
-router.post("/", auth, bookController.createBook);
-router.put("/:id", auth, bookController.modifyBook);
+router.post("/", auth, multer, bookController.createBook);
+router.put("/:id", auth, multer, bookController.modifyBook);
 router.delete("/:id", auth, bookController.deleteBook);
 router.get("/:id", bookController.getOneBook);
 router.get("/", bookController.getAllBooks);
+
+//new
+// router.get("/bestrating", bookController.getBestRatings);
+// router.post("/:id/rating", bookController.createRating);
 
 module.exports = router;
